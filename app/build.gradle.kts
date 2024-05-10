47e7d84e68b685c53d6b2b9e5da717b5711a3fff
SateaMall <sate3.mallouh@gmail.com>
2024-05-10 18:17:55 +0200

    adding location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("com.google.android.material:material:1.12.0")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
 
 
     // Import the Firebase BoM
